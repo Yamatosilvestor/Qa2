bebb88380354c7531abf5571ac5d08287b25babd
Yamatosilvestor <138121970+Yamatosilvestor@users.noreply.github.com>
2023-09-27 20:09:01 +0600

    Add files via upload

---
+++ QA2_Spring/src/test/java/com/example/qa/QaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qa;
+
+import com.example.qa.main.MainApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MainApplication.class) class QaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
